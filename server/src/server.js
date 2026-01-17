708251ba3142961682a7f66694a62ebbe16cf31c
Harshith P <harshith.p734@gmail.com>
2026-01-17 10:46:48 +0530

    chores: added cron job for seat unloacking

--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,24 @@
 import "./config/env.js";
 import app from "./app.js";
 import connectDB from "./config/db.js";
+import cron from "node-cron";
+import releaseExpiredLocks from "./jobs/releaseExpiredLocks.job.js";
+
 
 const PORT = process.env.PORT || 5000;
 
 const startServer = async () => {
   await connectDB();
+
+  // Run every minute
+  cron.schedule("* * * * *", async () => {
+    try {
+      await releaseExpiredLocks();
+    } catch (err) {
+      console.error("Cron error:", err.message);
+    }
+  });
+
 
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
