c35e17bb7082d318ba1976d64a42f0e2a2728572
me-lw-in <melwinmanish30@gmail.com>
2026-01-17 12:11:47 +0530

    fix(ui): correct Sidebar import path in layout components

--- client/src/layouts/SuperAdminLayout/SuperAdminLayout.jsx
+++ client/src/layouts/SuperAdminLayout/SuperAdminLayout.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./SuperAdminLayout.css";
 import Header from "../../components/Header/Header";
-import Sidebar from "../../components/Sidebar/SideBar";
+import Sidebar from "../../components/SideBar/SideBar";
 import { superAdminMenu } from "../../config/SideBarIcon";
 
 const SuperAdminLayout = () => {
